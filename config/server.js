9fb12e8c65ba20ebe6538e3274a0f15e1ddd590f
Jackson Alves <jacksonalves541@gmail.com>
2021-10-03 10:32:25 -0300

    first commit with init configuration

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a943eda36ccaff07ecdd03836cdc92a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64c4bb6b-20c2-4faf-b4cb-b9b099fdec48'
+};
